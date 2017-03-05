871bf9fa27c2396ac15c827ba0d0302bb49581bf
DoriusFox <lepumpgr@hotmail.com>
2017-03-05 23:07:17 +0300

    Version 1.1.7.3

--- EnhancedParser.cs
+++ EnhancedParser.cs
@@ -34,8 +34,8 @@ using System.Diagnostics;
 
 [assembly: AssemblyTitle("The Secret World parser for group damage, heal and tank statistics")]
 [assembly: AssemblyDescription("Read through the CombatLog.txt files and parse the combat and healing done (ACT3)")]
-[assembly: AssemblyCopyright("Author: Boorish, since 1.0.5.4 Lausi; Contributions from: Eafalas, Holok, Inkraja, Akamiko; ***")]
-[assembly: AssemblyVersion("1.0.7.1")]
+[assembly: AssemblyCopyright("Author of default parser: Boorish, since 1.0.5.4 Lausi; Contributions from: Eafalas, Holok, Inkraja, Akamiko; Enhanced version (since 1.1.7.1) made by Doriafox; ")]
+[assembly: AssemblyVersion("1.1.7.3)]
 // This plugin is based on the Rift3 plugin by Creub and Altuslumen.  Thanks guys :)
 // Fix for glance and penetrate hits fom Holok
 // Added Incoming Damage (takencrit%, takenpen&, ...) to chat export (Holok)
